b1f1b3696c38a0697bce9a2be64b8b585676a364
Matt Wilkin <mwilkin22@gmail.com>
2019-05-23 08:42:40 -0700

    fix more errors in edit-file.js.

--- edit-file.js
+++ edit-file.js
@@ -20,22 +20,40 @@ let sourceFile = args[2];
 //   console.log(args[i]);
 // }
 
+// fs.readFile(sourceFile, 'utf8', (err, data) => {
+//   if (err) throw err;
+//   console.log('data length', data.length);
+//   console.log(`Before ${data}`);
+//   let randomValue = Math.floor(Math.random()*100) +1;
+//    data = randomValue;
+
+//     fs.writeFile(sourceFile, data, (err) => {
+//       if (err) throw err;
+//       console.log('The file has been saved!');
+//     });
+
+//     fs.readFile(sourceFile, 'utf8', (err, data) => {
+//     if (err) throw err;
+//     console.log('data length', data.length);
+//     console.log(`After ${data}`);
+//   });
+// });
+
 fs.readFile(sourceFile, 'utf8', (err, data) => {
   if (err) throw err;
   console.log('data length', data.length);
   console.log(`Before ${data}`);
   let randomValue = Math.floor(Math.random()*100) +1;
-   data = randomValue;
+  data = randomValue;
 
     fs.writeFile(sourceFile, data, (err) => {
       if (err) throw err;
       console.log('The file has been saved!');
+      fs.readFile(sourceFile, 'utf8', (err, data) => {
+        if (err) throw err;
+        console.log('data length', data.length);
+        console.log(`After ${data}`);
     });
 
-    fs.readFile(sourceFile, 'utf8', (err, data) => {
-    if (err) throw err;
-    console.log('data length', data.length);
-    console.log(`After ${data}`);
   });
 });
-
