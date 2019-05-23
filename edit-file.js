4f00aecda52559144da60577d9235d8070cecc43
Matt Wilkin <mwilkin22@gmail.com>
2019-05-23 08:38:51 -0700

    fix errors in edit-file.js.

--- edit-file.js
+++ edit-file.js
@@ -3,37 +3,36 @@
 const fs = require('fs');
 
 
-// // Get args from command line
-// let args = process.argv;
+// Get args from command line
+let args = process.argv;
 
-// // Make sure used properly, error if not
-// if(args.length < 5){
-//   console.error('Not enough args');
-//   return; //read: end process, however that needs to happen
-// }
+// Make sure used properly, error if not
+if(args.length < 2){
+  console.error('Not enough args');
+  return; //read: end process, however that needs to happen
+}
 
-// let sourceFile = args[2];
-// let destFile = args[3];
-// let operation = args[4];
+let sourceFile = args[2];
+
 
 // // Else, do work
 // for(let i = 0; i < args.length; i++){
 //   console.log(args[i]);
 // }
 
-fs.readFile('./files/test.txt', 'utf8', (err, data) => {
+fs.readFile(sourceFile, 'utf8', (err, data) => {
   if (err) throw err;
   console.log('data length', data.length);
   console.log(`Before ${data}`);
   let randomValue = Math.floor(Math.random()*100) +1;
    data = randomValue;
 
-    fs.writeFile('./files/test.txt', data, (err) => {
+    fs.writeFile(sourceFile, data, (err) => {
       if (err) throw err;
       console.log('The file has been saved!');
     });
 
-    fs.readFile('./files/test.txt', 'utf8', (err, data) => {
+    fs.readFile(sourceFile, 'utf8', (err, data) => {
     if (err) throw err;
     console.log('data length', data.length);
     console.log(`After ${data}`);
