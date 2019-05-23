dc4d16168c2475fc9b894305b86a4ad97f37ab04
Matt Wilkin <mwilkin22@gmail.com>
2019-05-22 22:36:36 -0700

    working on promises, not working yet.

--- edit-file.js
+++ edit-file.js
@@ -2,6 +2,25 @@
 
 const fs = require('fs');
 
+
+// // Get args from command line
+// let args = process.argv;
+
+// // Make sure used properly, error if not
+// if(args.length < 5){
+//   console.error('Not enough args');
+//   return; //read: end process, however that needs to happen
+// }
+
+// let sourceFile = args[2];
+// let destFile = args[3];
+// let operation = args[4];
+
+// // Else, do work
+// for(let i = 0; i < args.length; i++){
+//   console.log(args[i]);
+// }
+
 fs.readFile('./files/test.txt', 'utf8', (err, data) => {
   if (err) throw err;
   console.log('data length', data.length);
